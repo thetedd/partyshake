99e9067d540d8509fe10d56e90cb9b84a4d5d3cb
Ted <theted@berkeley.edu>
2014-11-21 18:58:34 -0800

    asset shakedetectorgui

---
+++ Assets/shakeDetector.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var zAcc : Vector3;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI () {
+
+    GUI.Box (Rect (0,0,200,200), zAcc.ToString());
+    
+    GUI.Box (Rect (200,200,200,200), "ClassificationString here");
+    
+ }
