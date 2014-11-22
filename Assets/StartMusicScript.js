0399d6b7897562deb4df49adc1f168de7cb49f41
Hina Sakazaki <hinasakazaki@gmail.com>
2014-11-21 20:05:25 -0800

    STarting spectrum stuff

---
+++ Assets/StartMusicScript.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var guiCountDown : GUIText;
+var countMax : int; 
+private var countDown : int;
+
+function Start () {
+	guiCountDown.enabled = true;
+//	gameStart();
+}
+
+function Update () {
+
+}
+
+//function GameStart(){
+//	var song = gameObject.Find("Song");
+//	
+//	for (countDown = countMax; countDown > 0; countDown --){
+//		guiCountDown.text = countDown.ToString();
+//		yield WaitForSeconds(1);
+//	}
+//	
+//	guiCountDown.enabled = false;
+//}
+
---
+++ Assets/songScript.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var show : GUIText;
+
+@script RequireComponent(AudioSource)
+function Start () {
+	audio.Play();
+}
+
+function Update () {
+    var spectrum : float[] = audio.GetSpectrumData (1024, 0, FFTWindow.BlackmanHarris);
+    for (var i = 1; i < 1023; i++) {
+    	show.text = spectrum[0].ToString();
+
+	}
+}
